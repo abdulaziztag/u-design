5a8cdea29993692b54f42d3261302a402ddba91a
Abdulaziz <mannopov481@gmail.com>
2025-05-05 00:25:51 +0500

    feat: optimized build

--- src/components/Icon/Icon.type.ts
+++ src/components/Icon/Icon.type.ts
@@ -1,6 +1,7 @@
-import { FilledIconName } from '@components/Icon/icons-filled';
-import { OutlinedIconName } from '@components/Icon/icons-outlined';
 import React from 'react';
+
+import { FilledIconName } from './icons-filled';
+import { OutlinedIconName } from './icons-outlined';
 
 export interface UIconProps extends React.SVGProps<SVGSVGElement> {
   name: FilledIconName | OutlinedIconName;
